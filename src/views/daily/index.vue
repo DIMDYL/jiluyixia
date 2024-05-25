396381e0b959702dcc960f7a0988bee0ec4c544d
DIM云龙 <hello@z2z.cn>
2024-05-25 19:36:32 +0800

    添加日常页面

---
+++ src/views/daily/index.vue
@@ -0,0 +1,19 @@
+<script setup>
+import indexTop from './indexTop.vue'
+import story from '@/components/article/index.vue'
+</script>
+<template>
+  <div class="daily">
+    <indexTop />
+    <story></story>
+  </div>
+</template>
+<style lang="less" scoped>
+.daily {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 0 77px 0;
+}
+</style>
---
+++ src/views/daily/indexTop.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="dailytopimg">
+    <!-- <div class="topshadow"></div> -->
+    <img src="@/assets/007Hulo3gy1hpu3o0gda0j34mo334hdx.jpg" />
+    <div class="bottomshadow"></div>
+  </div>
+</template>
+<style lang="less" scoped>
+.dailytopimg {
+  width: 100%;
+  position: relative;
+  min-height: 200px;
+  max-height: 1000px;
+  overflow: hidden;
+  img {
+    width: 100%;
+    display: block;
+  }
+  .topshadow,
+  .bottomshadow {
+    width: 100%;
+    height: 200px;
+    position: absolute;
+  }
+  .topshadow {
+    top: 0;
+    background-image: -webkit-linear-gradient(
+      top,
+      rgba(0, 0, 0, 0.724),
+      rgba(0, 0, 0, 0.447) 30%,
+      rgba(0, 0, 0, 0) 100%
+    );
+    // filter: blur(10px); /* 根据需要调整模糊值 */
+  }
+  .bottomshadow {
+    bottom: 0;
+    background-image: -webkit-linear-gradient(
+      bottom,
+      rgb(0, 0, 0) 20%,
+      rgba(0, 0, 0, 0.648) 40%,
+      rgba(0, 0, 0, 0) 100%
+    );
+  }
+}
+</style>
